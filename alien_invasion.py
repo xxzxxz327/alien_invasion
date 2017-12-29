404f56a2257110e1569af8bd16b2bcfc38c7a0dc
xxzxxz <xxzxxz513@outlook.com>
2017-12-29 23:08:14 +0800

    基本积分，最高积分，等级“

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,10 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_function as gf
+from alien import Alien
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
@@ -14,19 +18,34 @@ def run_game():
         (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #创建Play按钮
+    play_button = Button(ai_settings,screen,"Play")
+
+    #创建一个用于存储游戏统计信息的实例,创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings,screen,stats)
+
     #创建一艘飞船
     ship = Ship(ai_settings,screen)
 
     #创建用于存储子弹的编组
     bullets = Group()
 
+    #创建一个外星人编组
+    aliens = Group()
+
+    #创建外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+
 
     #开始游戏主循环
     while True:
-        gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings,screen,stats,sb,play_button,ship,aliens,bullets)
+        if stats.game_active == True:
+            ship.update()
+            gf.update_bullets(ai_settings, screen,stats,sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats,sb,screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen,stats,sb, ship, aliens,bullets,play_button)
 
 
 
--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
 
-    def __init__(self,aisettings,screen):
+    def __init__(self,ai_settings,screen):
         """初始化飞船并设置其初始位置"""
+        super(Ship,self).__init__()
         self.screen = screen
-        self.ai_settings = aisettings
+        self.ai_settings = ai_settings
 
         #加载飞船图像并获取其外接矩形
         self.image = pygame.image.load("images/ship.bmp")
@@ -30,9 +32,13 @@ class Ship():
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
 
-        #根据self.center更新rect对象,cneterx只存储整数
+        #根据self.center更新rect对象,centerx只存储整数
         self.rect.centerx = self.center
 
     def blitme(self):
         """在指定的位置绘制飞船"""
         self.screen.blit(self.image,self.rect)
+
+    def center_ship(self):
+        """让飞船居中"""
+        self.center = self.screen_rect.centerx
